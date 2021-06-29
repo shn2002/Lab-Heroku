2d8953ca4fdcb0150e3fe33a7397a79503bcef89
shn2002 <joeshan001@gmail.com>
2021-06-29 12:43:51 -0400

    second commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const port = process.env.PORT || 3000
 const app = express();
 
 app.get('/', (req, res, next)=>{
     res.send("Hello Heroku!");
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Listening on port 3000')
 
 })
